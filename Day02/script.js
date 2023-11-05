b262e5450fb9a4526d60effeaa63a1dc32f4efb3
ArjunSingh757 <arjunsingh7743@gmail.com>
2023-11-05 08:29:56 +0530

    loginFontend+API

---
+++ Day02/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
+  const errorMessage = document.getElementById("error-message");
+
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // You should replace 'YOUR_API_URL' with the actual URL of your API
+    const apiUrl = "http://localhost:8088/login";
+    //localhost:8088/login
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // Successfully logged in, you can redirect to another page or perform other actions
+          alert("Login successful!");
+        } else {
+          errorMessage.textContent = "Invalid credentials. Please try again.";
+          errorMessage.classList.remove("hidden");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        errorMessage.textContent = "An error occurred. Please try again later.";
+        errorMessage.classList.remove("hidden");
+      });
+  });
+});
