14972d799fbc7defb43e5ad426a13e46e75a7b6f
ArjunSingh757 <arjunsingh7743@gmail.com>
2023-11-07 21:43:02 +0530

    Quize app

--- Day02/script.js
+++ Day02/script.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
   loginForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const username = document.getElementById("username").value;
+    const username = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    // You should replace 'YOUR_API_URL' with the actual URL of your API
+    // the actual URL of your API
     const apiUrl = "http://localhost:8088/login";
     //localhost:8088/login
 
